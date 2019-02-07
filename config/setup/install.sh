40a3907cafd785c1997710b691ddd29f1a488e41
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-02-07 11:10:04 -0600

    install file changes

--- config/setup/install.sh
+++ config/setup/install.sh
@@ -1,42 +1,143 @@
 #!/bin/bash
-echo -e "\nInstalling node:"
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo apt-get --assume-yes install xserver-xorg-video-fbturbo
+cat /dev/null > stele_install.log
 
-sudo apt-get --assume-yes install libgtk-3-0
+waitForNetwork ()
+{
+  if ! $(ping -c 1 -W 1 registry.nodejs.org > /dev/null 2>&1)
+  then
+    echo -e "\n** Waiting for network..."
+    while ! $(ping -c 1 -W 1 registry.nodejs.org > /dev/null 2>&1); do
+       echo "Still waiting..."
+       sleep 1
+    done
+    echo -e "\n** Network connected."
+  fi
+}
 
-sudo apt-get --assume-yes install git libudev-dev
+startWorking()
+{
+  workingText 2> /dev/null &
+  FEEDBACK_PID=$!
+}
 
-sudo apt-get --assume-yes install build-essential hostapd dnsmasq network-manager xserver-xorg xinit xserver-xorg-video-fbdev libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
+doneWorking()
+{
+  kill -1 $FEEDBACK_PID > /dev/null 2>/dev/null
+  sleep .5 &
+  wait $!
+}
 
-sudo apt-get --assume-yes install libasound2
+workingText()
+{
+  trap "echo -n $'\r''Working.... Done!'$'\n'; exit" SIGHUP
 
-while ! ping -c 1 -W 1 1.1.1.1; do
-    echo "Waiting for 1.1.1.1 - network interface might be down..."
-    sleep 1
-done
+  while [ 1 ]; do
+    for i in {0..4} ; do
+        echo -n $'\r''Working'
+        for ((j=0; j<i; j++)) ; do echo -n '.'; done
+        for ((j=0; j<4-i; j++)) ; do echo -n ' '; done
+        sleep .5 &
+        wait $!
+    done
+  done
+}
 
-echo  -e "\nClone the wrapper"
+handleError ()
+{
+  echo -e "\n**********************************************************"
+  echo -e "Error at line $1"
+  echo -e "\nCheck network connections and restart the install script."
+  echo -e "Also check stele_install.log for error information."
+  doneWorking
+}
 
-mkdir /usr/local/src/
+onExit()
+{
+  doneWorking
+}
+
+
+trap 'handleError $LINENO' ERR
+
+trap 'onExit' EXIT
+
+echo -e "\n* Starting stele-lite installation"
+
+if [ ! -f "${DIR}/passwordChanged" ]
+then
+  echo -e "\n** Set new password for user 'pi':"
+  passwd
+  sudo touch "${DIR}/passwordChanged"
+fi
+
+if [ -f "${DIR}/wpa_supplicant.conf" ]
+then
+  sudo echo "static domain_name_servers=1.1.1.1 1.0.0.1" >> /etc/dhcpcd.conf
+  echo -e "\n** Configuring Wifi..."
+
+  sudo mv "${DIR}/wpa_supplicant.conf" /etc/wpa_supplicant/wpa_supplicant.conf
+
+  sudo systemctl daemon-reload
+  sudo systemctl restart dhcpcd
+
+  waitForNetwork
+else
+  echo -e "\n** Wifi won't be configured."
+fi
+
+echo -e "\n** Installing node and system dependencies..."
+
+startWorking
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - > stele_install.log 2>&1
+
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install xserver-xorg-video-fbturbo > stele_install.log 2>&1
+
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install libgtk-3-0 > stele_install.log 2>&1
+
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install git libudev-dev > stele_install.log 2>&1
+
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install build-essential hostapd dnsmasq network-manager xserver-xorg xinit xserver-xorg-video-fbdev libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6  > stele_install.log 2>&1
+
+sudo apt-get -qq -o=Dpkg::Use-Pty=0 --assume-yes install libasound2 > stele_install.log 2>&1
+
+doneWorking
+
+echo -e "\nDone."
+
+echo  -e "\n** Checking directory structure..."
+
+sudo mkdir -p /usr/local/src
 
 sudo chmod 777 /usr/local/src
 
 cd /usr/local/src
 
-git clone --recurse-submodules https://github.com/scimusmn/stele-lite.git
-
-ln -s /usr/local/src/stele-lite ~/app
+if [[ ! -d "stele-lite" ]]; then
+  waitForNetwork
+  echo  -e "\n** Cloning the repository..."
+  git clone --recurse-submodules https://github.com/scimusmn/stele-lite.git
+  ln -s /usr/local/src/stele-lite ~/Application
+fi
 
 cd stele-lite
 
-echo  -e "\nInstalling dependencies for stele-lite:"
+echo  -e "\n** Installing node dependencies for stele-lite:"
 
-npm i
+startWorking
 
-echo  -e "\nConfiguring"
+## sometimes this call fails because it fails to dns registry.nodejs.org, retrying usually works
+while [ $(npm i 2> >( tee -a ~/stele_install.log | grep -o -i EAI_AGAIN) = 'EAI_AGAIN') ]; do
+  echo -e "\nDNS error while trying to install packages, retrying..."
+  waitForNetwork
+done
+
+doneWorking
+
+echo  -e "\n** Configuring machine..."
 
 cd configurator
 
